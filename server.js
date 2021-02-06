0e3d0faf1b5bf8c93f53d48941f38f707fff9c62
Vikash Shaw <vikashshaw893@gmail.com>
2021-02-06 17:22:56 +0530

    converting to class and object in server.js file

--- server.js
+++ server.js
@@ -6,30 +6,78 @@ const cors          = require("cors");
 const params        = require("./app/config/params.config")
 const db            = require("./app/models");
 
-const app = express();
+const app           = express();
 
-var corsOptions = {
+
+class Server {
+  
+  corsOptions = {
     origin: params.corsOptionsOriginUrls
-};
+  };
 
-app.use(cors(corsOptions));
+  constructor() {      
+      this.initExpressMiddleWare();
+      this.initCustomMiddleware();      
+      this.initRoutes();
+      this.start();
+  }
 
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
+  initExpressMiddleWare() {
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(cors(this.corsOptions));
 
-db.sequelize.sync();
+    // parse requests of content-type - application/json
+    app.use(bodyParser.json());
 
-// simple route
-app.get("/", (req, res) => {
-        res.json({ message: "Welcome to My application." });
+    // parse requests of content-type - application/x-www-form-urlencoded
+    app.use(bodyParser.urlencoded({ extended: true }));
+    
+    process.on('uncaughtException', (err) => {
+      if (err) console.log(err, err.stack);
+    });
+
+  }
+
+  initCustomMiddleware() {
+
+    db.sequelize.sync();
+
+    if (process.platform === "win32") {
+        require("readline").createInterface({
+            input: process.stdin,
+            output: process.stdout
+        }).on("SIGINT", () => {
+            console.log('SIGINT: Closing DB connection');
+            db.close();
+        });
+    }
+
+    process.on('SIGINT', () => {
+        console.log('SIGINT: Closing DB connection');
+        db.close();
+    });
+
+  }
+
+  initRoutes() {
+
+    // simple route
+    app.get("/", (req, res) => {
+      res.json({ message: "Welcome to My application." });
     })
 
-require("./app/routes/user.routes")(app);    
+    require("./app/routes/user.routes")(app);   
 
-// set port, listen for requests
-app.listen(params.PORT, () => {
-  console.log(`Server is running on port ${params.PORT}.`);
-});
+  }
+
+  start() {
+    // set port, listen for requests
+    app.listen(params.PORT, () => {
+      console.log(`Server is running on port ${params.PORT}.`);
+    });
+
+  } 
+
+}
+
+let server = new Server();
